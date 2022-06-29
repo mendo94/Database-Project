ac2d1fcef98133c041bcf6ae148c8506d2102907
mendo94 <rinamendozaa@gmail.com>
2022-06-29 09:35:44 -0400

    display user information on dashboard

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const app = express();
 const mustacheExpress = require("mustache-express");
 const session = require("express-session");
 const bcrypt = require("bcryptjs");
+// const formidable = require("formidable");
 const path = require("path");
 
 const VIEWS_PATH = path.join(__dirname, "/views");
+const __basedir = __dirname;
 global.models = require("./models");
-
-
 
 const PORT = 8080;
 app.engine("mustache", mustacheExpress(VIEWS_PATH + "/partials", ".mustache"));
-const userRouter = require("./routes/loginRegistration");
+
 app.use(
   session({
     secret: "somesecret",
@@ -21,7 +21,6 @@ app.use(
   })
 );
 app.use(express.urlencoded());
-app.use("/users", userRouter);
 app.set("views", VIEWS_PATH);
 app.set("view engine", "mustache");
 
@@ -29,15 +28,21 @@ app.use(express.static("static"));
 app.use("/js", express.static("static"));
 app.use("/css", express.static("static"));
 app.use("/img", express.static("static"));
+app.use("/uploads", express.static("static"));
 
 ///////////////////////////////////////////////////////////////
 //              Setup route for client side access
 ///////////////////////////////////////////////////////////////
+const userRouter = require("./routes/loginRegistration");
+
+app.use("/users", userRouter);
+
 const clientRoutes = require("./routes/clientInteraction");
 
 app.use("/client", clientRoutes);
 
 const objectRoutes = require("./routes/boxItemHandling");
+const formidable = require("formidable");
 
 app.use("/object-handling", objectRoutes);
 ///////////////////////////////////////////////////////////////
@@ -47,17 +52,54 @@ app.use("/object-handling", objectRoutes);
 //              DASHBOARD
 ///////////////////////////////////////////////////////////////
 
-// app.get("/homepage", async (req, res) => {
-//   const containers = await models.Container.findAll({});
+app.get("/edit-profile", async (req, res) => {
+  const id = req.session.user.userId;
+  const { username, first_name, last_name } = req.session.user;
 
-//   res.render("homepage", { containers: containers });
-// });
+  try {
+    const user = await models.User.findAll({
+      where: {
+        id: id,
+        username: username,
+        first_name: first_name,
+        last_name: last_name,
+      },
+    });
+    // res.json(user);
+    res.render("edit-profile", {
+      id: id,
+      username: username,
+      first_name: first_name,
+      last_name: last_name,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+function uploadFile(req, callback) {
+  new formidable.IncomingForm()
+    .parse(req)
+    .on("fileBegin", (name, file) => {
+      file.path = __basedir + "/uploads/" + file.name;
+    })
+    .on("file", (name, file) => {
+      callback(file.name);
+    });
+}
 
+app.post("/upload", (req, res) => {
+  uploadFile(req, (photoURL) => {
+    res.send("UPLOAD");
+  });
+});
 
 app.get("/homepage", async (req, res) => {
-  // res.json(item);
-  res.render("homepage");
+  const { first_name, last_name } = req.session.user;
+  res.render("homepage", {
+    first_name: first_name,
+    last_name: last_name,
+  });
 });
 
 app.post("/logout", (req, res) => {
@@ -74,13 +116,12 @@ app.get("/", (req, res) => {
 ///////////////////////////////////////////////////////////////
 
 // table for household
-app.get('/household-members', (req, res) => {
-  res.render('household-members')
-})
+app.get("/household-members", (req, res) => {
+  res.render("household-members");
+});
 
-app.get('/create-room', (req, res) => {
-  
-})
+app.get("/create-room", (req, res) => {});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
