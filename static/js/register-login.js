b75f8b07a57802db9cdb330d651f849782ce2c17
iamAlitkhan <khan.alitanveer@gmail.com>
2022-06-29 10:44:18 -0500

    password validation

--- static/js/register-login.js
+++ static/js/register-login.js
@@ -53,4 +53,15 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
+function matchPassword() {  
+  var pw1 = document.getElementById("pswd1");  
+  var pw2 = document.getElementById("pswd2");  
+  if(pw1 != pw2)  
+  {   
+    alert("Passwords did not match");  
+  } else {  
+    alert("Password created successfully");  
+  }  
+}  
+
 window.onload = openLoginModal;
